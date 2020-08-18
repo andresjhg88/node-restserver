c4746a9d1ce351503ab7e3dcfe7a7ad3a27a3412
Andres Gomez <andresgomez.andresjhg88@gmail.com>
2020-08-18 02:26:02 -0300

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -24,9 +24,11 @@ let urlDB
 if ( process.env.NODE_ENV === 'dev' ){
    urlDB =  'mongodb://localhost:27017/cafe'
 }else {
-   urlDB = 'mongodb+srv://a88:AUH5ozv3HLaRbvDl@cluster0-xdxcj.mongodb.net/test?retryWrites=true&w=majority'
+   urlDB = 'mongodb+srv://a88:AUH5ozv3HLaRbvDl@cluster0-xdxcj.mongodb.net/cafe?retryWrites=true&w=majority'
    
 }
 process.env.URLDB = urlDB;
 
 
+//mongodb+srv://a88:<password>@cluster0-xdxcj.mongodb.net/test
+//'mongodb+srv://a88:AUH5ozv3HLaRbvDl@cluster0-xdxcj.mongodb.net/test?retryWrites=true&w=majority'
--- server/server.js
+++ server/server.js
@@ -15,8 +15,7 @@ app.use(bodyParser.json())//app.use son midelwers hace referencia a funciones qu
 
 app.use( require('./rutes/usuario') );
 
-mongoose
-.connect(process.env.URLDB,{
+mongoose.connect(process.env.URLDB,{
 
   useNewUrlParser: true,
   useUnifiedTopology: true
@@ -27,5 +26,46 @@ mongoose
 
 
 app.listen(process.env.PORT, () => {
-    console.log( 'Escuchando el puerro:', process.env.PORT);
+    console.log( 'Escuchando el puerto:', process.env.PORT);
 });
+
+/**
+ * ***********************!!ATENCION!!***************************
+ *                                                              *
+ *     PARA HABILITAR EL SERVICIO LOCAL DE LA BASE DE DATOS     *
+ *                                                              *
+ *    UTILIZAR: 'mongodb://localhost:27017/cafe' EN LINEA 18    *
+ *                                                              *
+ *           SE REQUIERE TENER 2 CONSOLAS HABIERTAS             *
+ *                                                              *
+ * 1na CON EL CODIGO EN EJECUCION DE "nodemon server/server.js" *
+ *                                                              *
+ *              2da CON "sudo mongod" EN EJECUCION              *
+ *                                                              *
+ *  DE ESTA FORMA PODEMOS TRABAJAR LOCALMENRE CON EL POSTMAN    *
+ *                                                              *
+ ****************************************************************
+ */
+
+/**
+ * ***********************!!ATENCION!!***************************
+ *                                                              *
+ *    PARA HABILITAR EL SERVICIO ON-LINE EN LA BASE DE DATOS    *
+ *                                                              *
+ *           UTILIZAR: 'process.env.URLDB' EN LINEA 18          *
+ *                                                              *
+ *           SE REQUIERE TENER 2 CONSOLAS HABIERTAS             *
+ *                                                              *
+ * 1na CON EL CODIGO EN EJECUCION DE "nodemon server/server.js" *
+ *                                                              *
+ *              2da CON "sudo mongod" EN EJECUCION              *
+ *                                                              *
+ *  DE ESTA FORMA PODEMOS TRABAJAR ON-LINE CON EL POSTMAN       *
+ *                                                              *
+ * PARA CONCRETAR LA CONECCION DEVEMOS CONECTAR LA APLICACION   *
+ * DESDE MONGO ATLAS Y COPIAR LA URL QUE REQUIERE REEMPLAZANDO  *
+ * EL NOMBRE DEL USIARIO, PASWORD Y DBNAME QUE SE EXPECIFICA EN *
+ * EL ARCHIVO config.js.                                        *
+ *                                                              *
+ ****************************************************************
+ */
