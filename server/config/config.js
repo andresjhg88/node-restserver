10001ea41837fbabeb5d272439830bebf83fba9a
Andres Gomez <andresgomez.andresjhg88@gmail.com>
2021-01-15 00:08:07 -0300

    Productos y Categorias

--- server/config/config.js
+++ server/config/config.js
@@ -29,7 +29,7 @@ process.env.SEED = process.env.SEED || 'este-es-el-seed-de-desarrollo'
 // 60 minutos
 // 24 horas
 // 30 dias
-process.env.CADUCIDAD_TOKEN = 60 * 60 * 24 * 30;
+process.env.CADUCIDAD_TOKEN = '48h';
 
 
 //=================================
